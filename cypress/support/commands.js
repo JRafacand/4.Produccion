1c6ca9160669767d104ba3dd537ab86a8f1bf0b8
JRafaCand <r.candilejo@gmail.com>
2023-09-26 11:55:34 -0500

    update schedule task date

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.on('uncaught:exception', (err, runnable) => {//capturo uncaught:exception para que cyporess continue con la prueba 
+    return false
+  })
